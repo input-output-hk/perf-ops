ef0b103615bcca71510a37125ca4788cb39c0cab
John Lotoski <john.lotoski@iohk.io>
2020-03-06 09:46:32 -0500

    Adds ami-sync state rebuild, snapshot and image re-use

--- nix/pkgs/ami-sync/ami-sync.cr
+++ nix/pkgs/ami-sync/ami-sync.cr
@@ -15,6 +15,14 @@ ALL_REGIONS = %w[
 ]
 
 module Shell
+  def shSilent!(cmd, *args)
+    output = IO::Memory.new
+    Process.run(cmd, args, output: output, error: STDERR).tap do |status|
+      raise "#{cmd} #{args} failed" unless status.success?
+    end
+    output.to_s.strip
+  end
+
   def sh!(cmd, *args)
     puts "$ #{cmd} #{args.to_a.join(" ")}"
     output = IO::Memory.new
@@ -36,8 +44,6 @@ extend Shell
 class ImageInfo
 
   include Shell
-
-  # TODO: use http://169.254.169.254/latest/dynamic/instance-identity/document to get HOME_REGION
 
   BUCKET      = "iohk-amis"
 
@@ -91,10 +97,10 @@ class ImageInfo
     end
   end
 
-  def upload_all!(homeRegion, regions)
-    home_image_id = upload_image homeRegion
+  def upload_all!(homeRegion, regions, force)
+    home_image_id = upload_image(homeRegion, force)
     (regions - [ homeRegion ]).each do |region|
-      copied_image_id = copy_to_region region, homeRegion, home_image_id.not_nil!
+      copied_image_id = copy_to_region region, homeRegion, home_image_id.not_nil!, force
     end
   end
 
@@ -112,30 +118,44 @@ class ImageInfo
     end
   end
 
-  def upload_image(region)
-    upload_image_import(region)
-    upload_image_snapshot(region)
-    upload_image_deregister(region)
-    upload_image_register(region)
+  def upload_image(region, force)
+    upload_image_import(region, force)
+    upload_image_snapshot(region, force)
+    upload_image_deregister(region, force)
+    return upload_image_register(region, force)
   end
 
-  def upload_image_snapshot(region)
+  def upload_image_snapshot(region, force)
     with_image region do |image|
-      return if image.snapshot_id
+      return if !force && image.snapshot_id
 
       puts "Waiting for import"
 
+      # TODO: Add a Key=perf-ops Value=$(cat perf-uuid.txt) tag here
       image.snapshot_id = wait_for_import(region, image.task_id.not_nil!)
+      if ami_tag_output = sh!("aws", "ec2", "create-tags",
+        "--region", region,
+        "--resources", "#{image.snapshot_id}",
+        "--tags", "Key=Name,Value=\"#{name}\"")
+        puts "Snapshot tag successfully created"
+      else
+        puts "Snapshot tagging failed."
+      end
+      puts
     end
   end
 
-  def upload_image_deregister(region)
-    deregister_by_name(region)
+  def upload_image_deregister(region, force)
+    deregister_by_name(region, force)
   end
 
   # TODO: handle already existing image and allow setting name based on image hash
-  def upload_image_register(region)
+  def upload_image_register(region, force)
     with_image region do |image|
+      return image.ami_id if !force && image.ami_id
+      if force
+        puts "Forcing a new ami registration"
+      end
       ebs = {
         "SnapshotId"          => image.snapshot_id,
         "VolumeSize"          => logical_gigabytes,
@@ -161,34 +181,45 @@ class ImageInfo
         "--sriov-net-support", "simple",
         "--ena-support",
         "--virtualization-type", "hvm"
-
       result = RegisterImageResult.from_json(ami_id_output)
       image.ami_id = result.image_id
       puts "#{region} AMI ID: #{image.ami_id.to_s}"
-      image.ami_id
+      puts
+      return image.ami_id
     end
   end
 
-  def deregister_by_name(region)
+  def deregister_by_name(region, force)
     output = sh "aws", "ec2", "describe-images",
       "--region", region,
       "--filters", "Name=name,Values=\"#{name}\""
-
+    puts
     images = Hash(String, Array(ImageDescription)).from_json(output.to_s)
 
-    images["Images"].each do |image|
-      puts "deregister #{image.image_id} in #{region}"
-      sh! "aws", "ec2", "deregister-image",
-        "--region", region,
-        "--image-id", image.image_id
+    if force
+      images["Images"].each do |image|
+        puts "Deregister #{image.image_id} in #{region}"
+        sh! "aws", "ec2", "deregister-image",
+          "--region", region,
+          "--image-id", image.image_id
+        puts
+      end
+    else
+      images["Images"].each do |image|
+        puts "Updating the registry file with #{image.image_id} in #{region}"
+        with_image region do |imageRegistry|
+          imageRegistry.ami_id = image.image_id
+        end
+        puts
+      end
     end
   end
 
   def wait_for_import(region, task_id)
-    puts "waiting for import task #{task_id} to be completed"
+    puts "Waiting for import task #{task_id} to be completed"
 
     loop do
-      output = sh! "aws", "ec2", "describe-import-snapshot-tasks",
+      output = shSilent! "aws", "ec2", "describe-import-snapshot-tasks",
         "--region", region,
         "--import-task-ids", task_id
 
@@ -197,27 +228,55 @@ class ImageInfo
 
       case task.status
       when "active"
-        puts "%3s/100 : %s" % [task.progress, task.status_message]
+        print ("%4s/100 : %s" % [task.progress, task.status_message]).ljust(60)+"\r"
         sleep 10
       when "completed"
+        puts
+        puts
         return task.snapshot_id
       else
-        raise "unexpected snapshot import status for #{task_id}: #{task.inspect}"
+        puts
+        raise "Unexpected snapshot import status for #{task_id}: #{task.inspect}"
       end
     end
   end
 
-  def upload_image_import(region)
+  def upload_image_import(region, force)
     with_image region do |image|
-      puts "Checking for image on S3"
-      return if task_id = image.task_id
+      check_for_image(region, force)
+      puts
+      if !force
+        puts "Checking for an existing snapshot in the home region"
+        existing_snapshot = sh! "aws", "ec2", "describe-snapshots",
+          "--region", region, "--filters", "Name=tag:Name,Values=\"#{name}\""
 
-      check_for_image(region)
+        # TODO: sort the filter result to use the most recent image if multiple are found
+        # TODO: add tag of Key=perf-ops Value=$(cat perf-uuid.txt) to each snapshot for easier cleanup
+        # TODO: add snapshot and ami cleanup functions, utilizing tags for discrimination
+
+        # Check for an existing snapshot with the image hash; use it if it exists
+        snapshotMatch = SnapshotDescribe.from_json(existing_snapshot).matches
+        if snapshotMatch.size >= 1 && snapshotMatch.first.state == "completed"
+          if snapshotMatch.size == 1
+            puts "Found an existing snapshot with the expected hash in the home region.  " \
+                 "Using the existing snapshot: #{snapshotMatch.first.snapshotId}"
+          else
+            puts "Warning: multiple snapshots with the same hash found in the home region.  " \
+                 "Using the first snapshot found: #{snapshotMatch.first.snapshotId}"
+          end
+          image.snapshot_id = snapshotMatch.first.snapshotId
+          puts
+          return
+        end
+      else
+        puts "Force pushing a new image snapshot to ec2"
+      end
 
       puts "Importing image from S3 path #{s3_url}"
 
       task_id_output = sh! "aws", "ec2", "import-snapshot",
         "--region", region,
+        "--description", "#{name}",
         "--disk-container", {
         "Description" => "nixos-image-#{label}-#{system}",
         "Format"      => "vhd",
@@ -226,6 +285,7 @@ class ImageInfo
           "S3Key"    => s3_name,
         },
       }.to_json
+      puts
 
       if task_id = task_id_output
         image.task_id = ImportResult.from_json(task_id).import_task_id
@@ -233,17 +293,47 @@ class ImageInfo
     end
   end
 
-  def check_for_image(region)
-    return if sh "aws", "s3", "ls", "--region", region, s3_url
-    puts "Image missing from aws, uploading"
+  def check_for_image(region, force)
+    puts
+    if !force
+      puts "Checking for image on S3"
+      return if sh "aws", "s3", "ls", "--region", region, s3_url
+      puts "Image missing from aws s3, uploading"
+    else
+      puts "Force pushing the image to aws s3, uploading"
+    end
     sh "aws", "s3", "cp", "--region", region, file, s3_url
   end
 
-  def copy_to_region(region, from_region, from_ami_id)
-    pp! :copy_to_region, region, from_region, from_ami_id
+  def copy_to_region(region, from_region, from_ami_id, force)
+    # pp! :copy_to_region, region, from_region, from_ami_id
     with_image region do |image|
-      pp! image
-      return if ami_id = image.ami_id
+      # pp! image
+      if image.ami_id && !force
+        puts "Using an existing AMI for #{region} in the registry file: #{image.ami_id}"
+        return image.ami_id
+      elsif !force
+        output = sh "aws", "ec2", "describe-images",
+          "--region", region,
+          "--filters", "Name=name,Values=\"#{name}\""
+
+        # Check for an existing ami image with the hash name; use it if it exists
+        images = Hash(String, Array(ImageDescription)).from_json(output.to_s)
+        imagesMatch = images["Images"]
+        if imagesMatch.size >= 1 && imagesMatch.first.state == "available"
+          if imagesMatch.size == 1
+            puts "Found an existing ami image with the name hash in the region #{region}.  " \
+                 "Using the existing ami image: #{imagesMatch.first.image_id}"
+          else
+            puts "Warning: multiple ami images with the name hash found in the region #{region}.  " \
+                 "Using the first ami image found: #{imagesMatch.first.image_id}"
+          end
+          image.ami_id = imagesMatch.first.image_id
+          return ami_id = image.ami_id
+        end
+      else
+        puts "Forcing an ami image copy to region #{region}"
+      end
       ami_id_output = sh! "aws", "ec2", "copy-image",
         "--region", region,
         "--source-region", from_region,
@@ -252,13 +342,29 @@ class ImageInfo
         "--description", description
 
       image.ami_id = RegisterImageResult.from_json(ami_id_output).image_id
-      puts "#{region} AMI ID: #{image.ami_id.to_s}"
+      puts "Created AMI ID #{image.ami_id.to_s} in region #{region}"
     end
   end
 
   class RegisterImageResult
     JSON.mapping(
       image_id: {type: String, key: "ImageId"}
+    )
+  end
+
+  class SnapshotDescribe
+    class SnapshotResults
+      JSON.mapping(
+        state: { type: String, key: "State" },
+        snapshotId: { type: String, key: "SnapshotId" }
+      )
+    end
+
+    JSON.mapping(
+      matches: {
+        type: Array(SnapshotResults),
+        key: "Snapshots"
+      }
     )
   end
 
@@ -382,7 +488,9 @@ class ImageDescription
   end
 end
 
+# Main
 config = Hash(String, String).new
+force = false
 
 OptionParser.parse ARGV do |o|
   o.banner = "Usage: ami-sync [arguments]"
@@ -398,6 +506,8 @@ OptionParser.parse ARGV do |o|
     "The home (source) region to push AMI images to and then copy from into other target regions; " \
     "defaults to \"#{HOME_REGION}\" if not declared") \
     { |homeRegion| config["homeRegion"] = homeRegion }
+  o.on("-f", "--force", "Force push files, images, snapshots and ami registrations, " \
+    "even if they already exist.") { force = true }
   o.on("-h", "--help", "Show this help") { puts o; exit(0) }
 end
 
@@ -416,10 +526,10 @@ puts amis
 # Parse the --home CLI option
 puts
 if !config.has_key? "homeRegion"
-  puts "Using default home region of \"#{HOME_REGION}\""
+  puts "Using default home region of:"
   homeRegion = HOME_REGION
 else
-  puts "Using \"home\" arg from CLI for home region"
+  puts "Using \"home\" arg from CLI for home region:"
   homeRegion = config["homeRegion"]
 end
 puts homeRegion
@@ -441,6 +551,11 @@ else
 end
 puts regions
 
+if force
+  puts
+  puts "Force push CLI option selected"
+end
+
 puts
 puts "AMI images to build and push: #{amis}"
 puts
@@ -459,7 +574,7 @@ amis.each do |ami|
     Amazon Arch: #{image.amazon_arch}
   INFO
 
-  image.upload_all!(homeRegion, regions[ami])
+  image.upload_all!(homeRegion, regions[ami], force)
 
   puts
 end
