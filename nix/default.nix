d5efb4c80279537e7f2eac11049a2d6fbd3bc53f
John Lotoski <john.lotoski@iohk.io>
2020-03-27 14:34:08 -0400

    Bump jorm and clean up niv sources and default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -2,17 +2,10 @@
 with {
   overlay = self: super: {
     inherit (import sources.niv { }) niv;
-    inherit (import sources.cardano-wallet { gitrev = sources.cardano-wallet.rev; }) cardano-wallet-jormungandr;
     inherit (import sources.nixpkgs-unstable {}) crystal terraform;
-    inherit (import sources.iohk-nix { }) jormungandrLib;
     packages = self.callPackages ./packages.nix { inherit sources; };
-    jormungandrEnv = self.jormungandrLib.environments.itn_rewards_v1;
     terranix = self.callPackage sources.terranix {};
     ami-sync = self.callPackage ./pkgs/ami-sync {};
-
-    inherit ((import sources.jormungandr-nix { environment = "itn_rewards_v1"; }).scripts)
-      janalyze sendFunds delegateStake createStakePool checkTxStatus;
-    inherit (self.jormungandrLib.packages.master) jormungandr jcli;
   };
 };
 import sources.nixpkgs {
